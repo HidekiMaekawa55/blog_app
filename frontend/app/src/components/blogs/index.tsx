65feb4c373e1581cd70cf0509f85fc97c8b7e43e
HidekiMaekawa55 <koronhideki@icloud.com>
2024-02-04 14:42:36 +0900

    💄 ブログコンポーネントの作成

---
+++ frontend/app/src/components/blogs/index.tsx
@@ -0,0 +1,19 @@
+export const Blogs = async () => {
+  const res = await fetch("http://api:3000/blogs");
+  const blogs = await res.json();
+
+  return (
+    <div>
+      {blogs.map((blog) => {
+        return (
+          <div key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>{blog.content}</p>
+            <button>編集</button>
+            <button>削除</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
