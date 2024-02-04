aea870ccb3618bc0cc2334982f57aaf63df99c75
HidekiMaekawa55 <koronhideki@icloud.com>
2024-02-04 14:39:05 +0900

    🔧 フロントのサーバーからバックエンドサーバーにfetchするための設定

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -68,4 +68,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # NOTE: [ActionDispatch::HostAuthorization::DefaultResponseApp] Blocked hosts: api:3000
+  config.hosts << "api"
 end
